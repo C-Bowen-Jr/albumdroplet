824d46ae0b95911a618442dda8e3466b3896462a
Chris Bowen Jr <laylong@gmx.com>
2024-01-09 23:09:28 -0500

    Tweaks To UI And Error Handling Chore:  - API rate limit related sleep/retries  - "New" tag and font

--- droplet.py
+++ droplet.py
@@ -33,7 +33,7 @@ class albumImage:
         cache.close()
 
         # Rate limit buffer, increase if you get 503 responses
-        time.sleep(0.2)
+        time.sleep(0.25)
         
     def get(self):
         return self.image
@@ -44,11 +44,11 @@ class albumImage:
 def get_followed_bands():
     return json.load(open("bands.json"))
 
-# get_artist_blob ( artist_name as string )
+# get_artist_blob ( artist_name as string, attempt as int )
 # returns a json object from the musicbrainz.org api
 # The artist_name is cross referenced with bands.json to be kind and save on api 
-# calls since it's free
-def get_artist_blob(artist_name):
+# calls since it's free. If this 503s, pause and try again
+def get_artist_blob(artist_name, attempt):
     bands = get_followed_bands()
 
     for band in bands:
@@ -60,15 +60,16 @@ def get_artist_blob(artist_name):
 
     if response.status_code == 200:
         artist_object = response.json()
+        time.sleep(0.25)
         return artist_object
 
-    elif respnse.status_code == 503:
+    elif response.status_code == 503 and attempt < 1:
         print("Error: Failed to get general artist data.")
         print("Response 503: Server is down or you are being rate limited")
-        return
+        time.sleep(2)
+        return get_artist_blob(artist_name, 1)
 
-    print("Error: failed response from musicbrains.org API")
-    print(f"Response error code: {response.status_code}")
+    print(f"Response {response.status_code}")
     return
 
 # get_album_url ( album_id as string )
@@ -82,13 +83,12 @@ def get_album_url(album_id):
         album_info = response.json()
         return album_info["images"][0]["thumbnails"]["250"]
 
-    elif respnse.status_code == 503:
+    elif response.status_code == 503:
         print("Error: Failed to get album art.")
         print("Response 503: Server is down or you are being rate limited")
         return
     
-    print("Error: failed response from musicbrains.org API")
-    print(f"Response error code: {response.status_code}")
+    print(f"Response {response.status_code}")
     return
 
 # get_albums ( artist_blob as json )
@@ -139,27 +139,31 @@ def main():
 
     root = tk.Tk()
     root.title("Droplet")
-    root.geometry("1500x1000")
+    root.geometry("1524x1018")
     grid_entry = 0
     grid_images = []
 
     for band in bands:
         try:
-            artist = get_artist_blob(band["name"])
+            artist = get_artist_blob(band["name"], 0)
             albums = get_albums(artist)
             latest_id = get_latest_album(albums)["id"]
             
+            is_new = "NEW: "
             if is_cached(latest_id):
                 grid_images.append(cachedImage(latest_id).get())
+                is_new = ""
             else:
                 grid_images.append(albumImage(latest_id).get())
 
-            imagelab = tk.Label(root, image=grid_images[grid_entry])
+            imagelab = tk.Label(root, text=f"{is_new}{band['name']}", fg = "white", bg ="black", wraplength = 220, font = ('Arial', 24, 'bold'), image=grid_images[grid_entry], compound='center')
             imagelab.grid(row = grid_entry // 6, column = grid_entry % 6)
             grid_entry += 1
-
-        except Exception:
-            print(f"^^^ Error occured for {band['name']}")
+        
+        except TypeError:
+            pass
+        except Exception as inst:
+            print(f"^^^ Error occured for {band['name']} because of {inst}")
 
     root.mainloop()
 
